cec0293204e97d82956f07018a2b4d3a0682b94f
FYKANG <f963706245@126.com>
2017-07-02 03:08:19 +0800

    routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function() {
+    return 'test2';
+});
+
+Route::get('/basic1', function () {
+    return 'welcome';
+});
