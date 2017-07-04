6e8b77a61c7355eaa2c6a4c18d63edd85c9b259d
FYKANG <f963706245@126.com>
2017-07-04 21:43:03 +0800

    qrcode

--- resources/views/owner/mysql.blade.php
+++ resources/views/owner/mysql.blade.php
@@ -5,12 +5,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>test</title>
 	<link rel="stylesheet" href="{{ URL::asset('css/test.css') }}" type="text/css">
+	<script src="/javascripts/application.js" type="text/javascript" charset="utf-8" async defer></script>
 </head>
 <body>
 <div class="test">
 @foreach($workout as $val)
           <p >{{$val->userName}}</p>
 @endforeach
+
+</div>
 </div>
 </body>
 </html>
